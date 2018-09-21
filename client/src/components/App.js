900e2182c70d9ab11550ba8449ecc5370744c3b7
David Jungst <djungst@gmail.com>
2018-09-21 09:52:37 -0600

    remove registration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import NoMatch from './NoMatch';
 import NavBar from './NavBar';
 import Login from './Login';
-import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
 import ProtectedRoute from './ProtectedRoute';
@@ -20,7 +19,6 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <AuthRoute exact path='/login' component={Login} />
-            <AuthRoute exact path='/register' component={Register} />
             <Route component={NoMatch} />
           </Switch>
         </FetchUser>
