1fb9e09d107797a6c2bd97b068d6a491d2398a7b
David Jungst <djungst@gmail.com>
2018-09-22 12:00:20 -0600

    fix bug where users have mult enrollments

--- app/models/course.rb
+++ app/models/course.rb
@@ -26,28 +26,27 @@ class Course < ApplicationRecord
       HTTParty::Basement.default_options.update(verify: false)
       url = "#{ENV['CANVAS_BASE_URL']}/courses/#{id}"
       auth = {"Authorization" => "Bearer #{ENV['CANVAS_API_KEY']}"}
-      Course.find_or_create_by(canvas_id: id) do |course|
-        canvas_course = HTTParty.get(url, headers: auth)
-        course.name = canvas_course['name']
-        counts[:name] = course.name
-        course.save
-        users_url = "#{ENV['CANVAS_BASE_URL']}/courses/#{id}/users?per_page=100"
-        users = HTTParty.get(users_url, headers: auth, query: { include: ['avatar_url', 'enrollments'] })
-        users.each do |u|
-          User.find_or_create_by(email: u['login_id']) do |user|
-            if user.new_record?
-              counts[:users] += 1
-              user.password = SecureRandom.hex
-            end
-            user.image = u['avatar_url']
-            user.name = u['name']
-            user.save!
-            enrollment = u['enrollments'].find { |e| e['course_id'] == id }
-            role = enrollment['role'].downcase.gsub('enrollment', '')
-            Enrollment.find_or_create_by(role: role, user_id: user.id, course_id: course.id) do |en|
-              counts[:enrollments] += 1 if en.new_record?
-            end
-          end
+      course = Course.find_or_create_by(canvas_id: id)
+      canvas_course = HTTParty.get(url, headers: auth)
+      course.name = canvas_course['name']
+      counts[:name] = course.name
+      course.save
+      users_url = "#{ENV['CANVAS_BASE_URL']}/courses/#{id}/users?per_page=100"
+      users = HTTParty.get(users_url, headers: auth, query: { include: ['avatar_url', 'enrollments'] })
+      users.each do |u|
+        binding.pry if u['login_id'] == 'dave@devpointlabs.com'
+        user = User.find_or_create_by(email: u['login_id'])
+        if user.new_record?
+          counts[:users] += 1
+          user.password = SecureRandom.hex
+        end
+        user.image = u['avatar_url']
+        user.name = u['name']
+        user.save!
+        enrollment = u['enrollments'].find { |e| e['course_id'] == id }
+        role = enrollment['role'].downcase.gsub('enrollment', '')
+        Enrollment.find_or_create_by(role: role, user_id: user.id, course_id: course.id) do |en|
+          counts[:enrollments] += 1 if en.new_record?
         end
       end
       counts
