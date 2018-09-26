ffcad2305012d76950e2a6349810ba60bcacdeed
David Jungst <djungst@gmail.com>
2018-09-25 20:32:58 -0600

    defaults for grade weight

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,18 @@
 class Course < ApplicationRecord
+  DEFAULT_ATTENDANCE = 75
+  DEFAULT_ASSIGNMENT = 25
   acts_as_paranoid
+  after_create :create_weights
 
   has_many :enrollments, dependent: :destroy
   has_many :users, through: :enrollments
   has_many :records, dependent: :destroy
   has_one :grade_weight, dependent: :destroy
+
+  def create_weights
+    self.create_grade_weight(attendance: DEFAULT_ATTENDANCE, assignments: DEFAULT_ASSIGNMENT)
+    self.save
+  end
 
   def self.with_enrollments(course_id)
     select('e.id, u.name, u.image, e.role, e.user_id')
