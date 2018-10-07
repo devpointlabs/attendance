3b2e5378940833c59353ef8a62566a679725676c
David Jungst <djungst@gmail.com>
2018-10-07 09:38:00 -0600

    remove warnings

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Segment, Form, Button } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { handleLogin } from '../reducers/user';
 import { Text, AuthContainer, AuthWrapper, Field, CommonButton } from './CommonStyles';
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Segment, Form, Button } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { setFlash } from '../reducers/flash';
