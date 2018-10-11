f8cbc0ec5b8b7fc24e860f015ba219e797020d8f
David Jungst <djungst@gmail.com>
2018-10-11 06:32:40 -0600

    set up mailer for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,14 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: 587,
+    domain: 'my.devpointlabs.com',
+    user_name: ENV['MAIL_LOGIN'],
+    password: ENV['MAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
