0423b06b8673ed389471fbc7db2e862101b03aad
David Jungst <djungst@gmail.com>
2018-10-11 13:16:41 -0600

    use domain for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com', 
     port: 587,
-    domain: 'my.devpointlabs.com',
+    domain: 'devpointlabs.com',
     user_name: ENV['MAIL_LOGIN'],
     password: ENV['MAIL_PASSWORD'],
     authentication: 'plain',
